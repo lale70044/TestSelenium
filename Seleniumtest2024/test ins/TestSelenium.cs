07cdf8b605a37a48c5781e6575c241d7f9f0c40d
lale7 <lale7@DESKTOP-RVKAHFH>
2024-01-10 18:01:08 +0400

    Add project files.

---
+++ Seleniumtest2024/test ins/TestSelenium.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seleniumtest2024.test_ins
+{
+    internal class TestSelenium
+    {
+    }
+}
